18c1ff900315891f2bb7e5d7f1aa7a1bde3c5bda
Andika Andriana <andriana.mia1@gmail.com>
2019-04-20 22:05:13 +0700

    Mengenal alert, prompt danconfirm pada JS

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert('hello word');
+alert('Selamat datang di halaman pembelajaran');
+var lagi = true;
+
+while(lagi === true){
+  var nama = prompt('Mohon masukan nama anda: ');
+  alert('Hi '+nama);
+
+  lagi = confirm('Ingin coba lagi? '+nama);
+}
+alert('Terima kasih telah berkunjung');
