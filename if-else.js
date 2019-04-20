a79937013d8336a70a242a71186b9af3704c2ee1
Andika Andriana <andriana.mia1@gmail.com>
2019-04-21 00:14:51 +0700

    Belajar pengulangan for dgn kondisi if-else

---
+++ if-else.js
@@ -0,0 +1,11 @@
+var jmlAngkot = 10;
+    angkotBeroperasi = 6;
+    noAngkot = 1;
+
+for(noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++){
+  if(noAngkot <= angkotBeroperasi){
+    console.log('Angkot No. '+noAngkot+' berfungsi dengan baik');
+  }else{
+    console.log('Angkot No. '+noAngkot+' tidak berfungsi dengan baik');
+  }
+}
