f3d3215f06b3fb1df77fcbfb749cc281b6016416
Andika Andriana <andriana.mia1@gmail.com>
2019-04-20 23:56:03 +0700

    Belajar for: Pengulangan di JS

---
+++ for.js
@@ -0,0 +1,11 @@
+var jmlAngkot = 10;
+    angkotBeroperasi = 6;
+    noAngkot = 1;
+    
+while(noAngkot <= angkotBeroperasi){
+  console.log('Angkot No. '+noAngkot+' Beroperasi dengan baik');
+  noAngkot++;
+}
+for(var angkotSisa = 7; angkotSisa <= jmlAngkot; angkotSisa++){
+  console.log('Angkot No. '+angkotSisa+' Sedang tidak beroperasi');
+}
